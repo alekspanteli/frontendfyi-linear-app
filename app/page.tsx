46d55075b41438db6c6473120c08ab1ee2534ff5
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2023-05-15 14:39:38 +0000

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,6 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main className="container text-white">
-      <h1 className="text-size-1 mb-2">Title goes here</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-    </main>
+    <div>test</div>
   );
 }
