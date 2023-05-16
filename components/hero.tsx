610b3bf6387bc2e5f43dc7ff673550e5f0c56fc9
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2023-05-16 14:11:27 +0000

    update

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,11 +7,11 @@ interface HeroElementProps {
 }
 
 export const HeroTitle = ({ children }: HeroElementProps) => {
-  return <h1 className="text-5xl/none">{children}</h1>;
+  return <h1 className="text-5xl/none my-5">{children}</h1>;
 };
 
 export const HeroSubtitle = ({ children }: HeroElementProps) => {
-  return <p className="text-lg/[1.3] mb-1">{children}</p>;
+  return <p className="text-lg/[1.3]">{children}</p>;
 };
 
 export const Hero = ({ children }: HeroPros) => {
