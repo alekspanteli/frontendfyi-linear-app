9280d6d0518bb43d734b1ddebe0a82b1fffad314
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2023-05-16 09:54:02 +0000

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+function px(pixels) {
+  return `${pixels / 16}rem`;
+}
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-inter)'],
-        mono: ['var(--font-roboto-mono)'],
-      },
-      colors: {
-        background: "rgb(var(--background-bg))"
-      },
-      fontSize: {
-        'size-0': 'clamp(2.75rem, 1.97rem + 3.91vw, 5.00rem)',
-        'size-1': ['clamp(2.75rem, 1.97rem + 3.91vw, 5.00rem)', '1.1'],
-      },
+    fontFamily: {
+      sans: ["var(--font-inter)"],
+      mono: ["var(--font-roboto-mono)"],
+    },
+    colors: {
+      background: "rgb(0 2 18 / .5)",
+      white: "rgb(255 255 255 / .5)",
+    },
+    fontSize: {
+      // "size-0": "clamp(2.75rem, 1.97rem + 3.91vw, 5.00rem)",
+      // "size-1": ["clamp(2.75rem, 1.97rem + 3.91vw, 5.00rem)", "1.1"],
+      "lg": px(16),
+      "5xl": px(80),
     },
   },
   corePlugins: {
     container: false,
-  }
-}
+  },
+};
