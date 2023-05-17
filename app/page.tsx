7884e8afa90375a1a418837d7c2cb643812d512a
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2023-05-17 14:42:01 +0000

    add image

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { Container } from "@/components/container";
 import { Hero, HeroSubtitle, HeroTitle } from "@/components/hero";
+import Image from "next/image";
+import heroImg from '@/public/hero.webp';
 
 export const metadata = {
   title: "Test",
@@ -20,6 +22,10 @@ export default function Homepage() {
               Meet the new standard for modern software development. Streamline
               issues, sprints, and product roadmaps.
             </HeroSubtitle>
+            <Image
+              src={heroImg}
+              alt="Picture of the author"
+            />
           </Hero>
         </Container>
       </main>
