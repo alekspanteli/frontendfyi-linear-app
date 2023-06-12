fa4ad5a406e208d1292efdf012d6b60062472aca
Alex Panteli <alekspanteli@gmail.com>
2023-06-12 10:15:11 +0300

    variable navbar rename

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ import { Logo } from "@/icons/logo";
 export const Header = () => {
   return (
     <header className="fixed left-0 top-0 w-full">
-      <Container className="relative flex h-[--_navbar-height] items-center gap-6 border-b border-white-a08">
+      <Container className="relative flex h-[--navbar-height] items-center gap-6 border-b border-white-a08">
         <Link href="/">
           <Logo />
         </Link>
