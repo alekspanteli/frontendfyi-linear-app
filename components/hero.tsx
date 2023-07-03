0aaf1c115921671883372966b3f0fe25416cdaa4
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2023-07-03 08:38:16 +0000

    update

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,7 +8,7 @@ interface HeroElementProps {
 
 export const HeroTitle = ({ children }: HeroElementProps) => {
   return (
-    <h1 className="text-balance mx-auto my-6 max-w-[18ch] text-5xl/none font-mono">
+    <h1 className="text-balance mx-auto my-6 max-w-[18ch] text-5xl/none">
       {children}
     </h1>
   );
